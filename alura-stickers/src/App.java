6221288fcd90686727416b811bf5958058ac45f8
Kevyn Gorito <kevy4job@gmail.com>
2022-07-18 16:06:39 -0300

    Exibindo os elementos extraídos, utilizando um foreach do tipo Map<String, String> em cima da lista de filmes e pegando suas chaves title, image e imDbRating, respectivamente

--- alura-stickers/src/App.java
+++ alura-stickers/src/App.java
@@ -25,7 +25,12 @@ public class App {
       
         // Exibir e manipular os dados
 
-        
+        for (Map<String,String> filme : listaDeFilmes) {
+            System.out.println(filme.get("title"));
+            System.out.println(filme.get("image"));
+            System.out.println(filme.get("imDbRating"));
+            System.out.println();
+        }
 
     }
 }
