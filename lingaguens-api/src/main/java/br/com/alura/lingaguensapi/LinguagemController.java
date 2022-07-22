1a398d4c6fe072cd260d02d75f91815d502efa96
Kevyn Gorito <kevy4job@gmail.com>
2022-07-22 17:31:46 -0300

    Resolvendo problema de nomenclatura do BD e conexão com mongoDB

--- lingaguens-api/src/main/java/br/com/alura/lingaguensapi/LinguagemController.java
+++ lingaguens-api/src/main/java/br/com/alura/lingaguensapi/LinguagemController.java
@@ -1,6 +1,5 @@
 package br.com.alura.lingaguensapi;
 
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -8,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
